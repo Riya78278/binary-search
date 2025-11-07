99605249239157e285cc49db0584199a31a59030
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-07 23:38:16 +0530

    Add files via upload

---
+++ bs9.cpp
@@ -0,0 +1,39 @@
+//  Search in Rotated Sorted Array (unique) 
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Check if mid is the target
+            if (nums[mid] == target) {
+                return mid; // Return the index of target
+            }
+            
+            // Identify which side is sorted
+            if (nums[left] <= nums[mid]) {
+                // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // Target is in the left half
+                } else {
+                    left = mid + 1; // Target is in the right half
+                }
+            } else {
+                // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // Target is in the right half
+                } else {
+                    right = mid - 1; // Target is in the left half
+                }
+            }
+        }
+        
+        return -1; // If the target is not found
+    }
+};

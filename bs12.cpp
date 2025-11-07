fa781c6279f7ac454a3e296c18b154e7bf900635
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-08 01:34:06 +0530

    Add files via upload

---
+++ bs12.cpp
@@ -0,0 +1,43 @@
+// Find Kth Rotation
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        int index=-1;
+        int ans=INT_MAX;
+        while(low <= high){
+            int mid=low+(high-low)/2;
+            
+            if(arr[low]<= arr[high]){
+                if(arr[low]<ans){
+                    index=low;
+                    ans=arr[low];
+                }
+                break;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<ans){
+                    index=low;
+                    ans=arr[low];
+                }
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+                if(arr[mid]<ans){
+                    index=mid;
+                    ans=arr[mid];
+                }
+            }
+            
+            
+        }
+        return index;
+    }
+};
